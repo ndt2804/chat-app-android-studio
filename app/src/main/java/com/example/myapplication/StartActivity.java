eca6ff67ef2abdd1758b1eed3b11a97e9934cf05
ndt2804 <nduytan.uit.a3@gmail.com>
2022-08-10 14:06:40 +0700

    continue

--- app/src/main/java/com/example/myapplication/StartActivity.java
+++ app/src/main/java/com/example/myapplication/StartActivity.java
@@ -7,13 +7,35 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class StartActivity extends AppCompatActivity {
     Button login, register;
+
+    FirebaseUser firebaseUser;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+
+
+        //check user null
+        if(firebaseUser != null)
+        {
+            Intent intent = new Intent(StartActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
 
         login = findViewById(R.id.login);
         register = findViewById(R.id.register);
